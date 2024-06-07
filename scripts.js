04e0f19a839b7c2e807f6140341d6962dc265aea
Sponono-creato <funiwengwenya@gmail.com>
2024-06-07 22:18:15 +0200

    Add files via upload

---
+++ scripts.js
@@ -0,0 +1,14 @@
+document.addEventListener('DOMContentLoaded', function() {
+    console.log('Document loaded');
+    
+    const navLinks = document.querySelectorAll('nav ul li a');
+    navLinks.forEach(link => {
+        link.addEventListener('click', function(event) {
+            event.preventDefault();
+            const targetId = this.getAttribute('href').substring(1);
+            document.getElementById(targetId).scrollIntoView({
+                behavior: 'smooth'
+            });
+        });
+    });
+});
